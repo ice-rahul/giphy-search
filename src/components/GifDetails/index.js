39d8eea1cb042ce5c078547a6300184ae2bc7453
ice-rahul <meet4g@gmail.com>
2021-06-10 23:37:59 +0530

    Copy Embed URL added

--- src/components/GifDetails/index.js
+++ src/components/GifDetails/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import getDateString from 'utils/getDateString';
 import * as COLORS from 'constants/Colors';
 import { Button, Share } from 'components';
@@ -6,11 +6,17 @@ import PropTypes from 'prop-types';
 import './GifDetails.scss';
 import getGif from 'utils/getGif';
 import getVideo from 'utils/getVideo';
+import getEmbedURL from 'utils/getEmbedURL';
 import downloadFile from 'utils/downloadFile';
 
 function GifDetails({
   className, gifId, shareUrl, title, publishedDate,
 }) {
+  const [status, setStatus] = useState('');
+  const handleCopyUrl = () => {
+    navigator.clipboard.writeText(getEmbedURL(gifId));
+    setStatus('Embed URL copied to clipboard ');
+  };
   return (
     <div className={`bg-white d-flex flex-column ml-5px mr-5px padding-10px text-left GifDetails ${className || ''}`}>
       <span className="font-1-2-em font-bold padding-10px pb-0">{title}</span>
@@ -22,7 +28,7 @@ function GifDetails({
         className="margin-5px hide-xs-350px"
         text="Download Gif &#x21E9;"
         textColor={COLORS.WHITE}
-        onClick={() => downloadFile(getGif(gifId), 'Download Gif')}
+        onClick={() => downloadFile(getGif(gifId), `Download Gif - ${gifId}`)}
       />
 
       <Button
@@ -30,13 +36,17 @@ function GifDetails({
         className="margin-5px hide-xs-350px"
         text="Download Mp4 &#x21E9;"
         textColor={COLORS.WHITE}
-        onClick={() => downloadFile(getVideo(gifId), 'Download Mp4')}
+        onClick={() => downloadFile(getVideo(gifId), `Download Mp4 - ${gifId}`)}
       />
 
       <Button
         className="margin-5px"
         text="Copy Embed URL"
+        onClick={handleCopyUrl}
       />
+      <div className="text-center color-red font-bold">
+        {status}
+      </div>
       <span className="border-left-4px font-bold margin-10px padding-5px">
         Share Gif
       </span>
